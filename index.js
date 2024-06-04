e20b9843484509cce0f5996110f33a39c1f95f1e
Antor4q <tariquelislam2015@gmail.com>
2024-06-04 23:51:12 +0600

    delete specific class by id done

--- index.js
+++ index.js
@@ -152,6 +152,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete("/teachers/:id",async(req,res) => {
+      const id = req.params.id
+      const query = { _id : new ObjectId(id)}
+      console.log('inside server teacher:', query)
+      const result = await classesCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
